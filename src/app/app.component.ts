4a44c97e690eb1bbb789ede8a8f3384e07529678
Jun Low <zhijun.low@gmail.com>
2024-03-08 18:14:19 +0800

    test: add unit test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MathGameComponent } from './components/math-game/math-game.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/components/math-game/math-game.component.spec.ts
+++ src/app/components/math-game/math-game.component.spec.ts
@@ -2,6 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MathGameComponent } from './math-game.component';
 import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('MathGameComponent', () => {
   let component: MathGameComponent;
@@ -9,7 +14,7 @@ describe('MathGameComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MathGameComponent]
+      imports: [MathGameComponent, FormsModule, MatInputModule, MatButtonModule, MatIconModule, BrowserAnimationsModule]
     })
     .compileComponents();
     
